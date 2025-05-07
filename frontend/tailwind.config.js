02c024cb46a9caee05a9f3d927938d893ac8858f
ajiang <ajiang42@buffalo.edu>
2025-05-07 11:16:28 -0400

    safelist

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'bg-blue-50',
+    'text-red-500',
+    'text-4xl',
+    'font-bold',
+    'p-10',
+    'text-center',
+    'min-h-screen',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
